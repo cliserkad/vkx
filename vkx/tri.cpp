cc80606ad2aaa18ea978b2aedce0fe0f7c09552e
cliserkad <bryanchristopherjohnson@gmail.com>
2020-12-05 19:59:54 -0500

    Add tri.cpp

---
+++ vkx/tri.cpp
@@ -0,0 +1,56 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+// default window size of 720p
+const uint32_t DEFAULT_WINDOW_WIDTH = 1280;
+const uint32_t DEFAULT_WINDOW_HEIGHT = 720;
+
+class SingleTriangleApp {
+public:
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	GLFWwindow* window;
+
+	void initWindow() {
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, "SingleTriangleApp", nullptr, nullptr);
+	}
+	void initVulkan() {
+
+	}
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
+	void cleanup() {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+};
+
+int main() {
+	SingleTriangleApp app;
+
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
